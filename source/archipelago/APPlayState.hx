734b1bfc33942b910f52da9908a6018e081bc0c2
Yutamon <79994593+Yuta12342@users.noreply.github.com>
2025-04-13 16:47:54 -0400

    Update APPlayState.hx

--- source/archipelago/APPlayState.hx
+++ source/archipelago/APPlayState.hx
@@ -1497,7 +1497,7 @@ class APPlayState extends PlayState {
     private override function generateSong():Void
     {
         super.generateSong();
-        if (PlayState.SONG == null || archipelago.APItem.activeItem.name=="Tutorial Trap") return;
+        if (PlayState.SONG == null || archipelago.APItem.activeItem?.name=="Tutorial Trap") return;
         apNotes = archipelago.APNote.replaceInQueue(playerField.noteQueue, apGame.excludeCheckedLocations(apGame.noteData(PlayState.SONG.song, currentMod)));
 
         for (field in playfields.members)
