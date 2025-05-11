898313e0275d6231a0cf9a2ac5689e7e3c3fbd43
Yutamon <79994593+Yuta12342@users.noreply.github.com>
2025-05-11 19:37:25 -0400

    Update SongBox.hx

--- source/states/freeplay/osu/SongBox.hx
+++ source/states/freeplay/osu/SongBox.hx
@@ -17,3 +17,39 @@ class DiffBox extends SongBox
     public var difID:Int = 0;
     public var difName:String = '';
 }
+
+
+class SongGroup extends FlxTypedGroup<SongBox>
+{
+    public var songBox:SongBox;
+    public var diffBoxes:Array<DiffBox>;
+
+    public function new(songBox:SongBox, diffNames:Array<String>)
+    {
+        super();
+        this.songBox = songBox;
+        this.diffBoxes = [];
+
+        add(songBox);
+
+        for (i in 0...diffNames.length)
+        {
+            var diffBox = new DiffBox();
+            diffBox.difID = i;
+            diffBox.difName = diffNames[i];
+            diffBox.posY = i + 1; // Position below the SongBox
+            diffBox.songID = songBox.songID;
+            diffBoxes.push(diffBox);
+            add(diffBox);
+        }
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+        for (diffBox in diffBoxes)
+        {
+            diffBox.posY = diffBoxes.indexOf(diffBox) + 1;
+        }
+    }
+}
