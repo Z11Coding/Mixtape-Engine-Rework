9b3a13cafa720717d070f5fccfbc751b8fb822a9
Yutamon <79994593+Yuta12342@users.noreply.github.com>
2025-05-11 00:48:29 -0400

    Update SongBox.hx

--- source/states/freeplay/osu/SongBox.hx
+++ source/states/freeplay/osu/SongBox.hx
@@ -3,6 +3,7 @@ package states.freeplay.osu;
 class SongBox extends FlxSprite
 {
     public var posY:Float = 0;
+    public var songID:Int = 0;
 
     override function update(elapsed:Float):Void {
         var targetY = FlxMath.lerp(y, (FlxG.height - height) / 2 + posY * 82, CoolUtil.boundTo(elapsed * 9, 0, 1));
