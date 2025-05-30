972d1588f2e9d2b8d2186dfaa671b7d90178fb39
Yutamon <79994593+Yuta12342@users.noreply.github.com>
2025-05-30 17:15:18 -0400

    Create AtlasDesigner.hx

---
+++ source/yutautil/AtlasDesigner.hx
@@ -0,0 +1,75 @@
+package yutautil;
+
+import haxe.io.Path;
+import sys.FileSystem;
+import sys.io.File;
+import lime.graphics.Image;
+import lime.graphics.ImageBuffer;
+import lime.utils.Bytes;
+import haxe.xml.Printer;
+
+
+// WIP// A simple atlas designer for packing images into a single spritesheet
+class AtlasDesigner {
+    public var images:Array<{name:String, image:Image, x:Int, y:Int, width:Int, height:Int}> = [];
+    public var sheetWidth:Int;
+    public var sheetHeight:Int;
+
+    public function new() {}
+
+    public function loadImagesFromFolder(folder:String):Void {
+        images = [];
+        var files = FileSystem.readDirectory(folder);
+        for (file in files) {
+            var ext = Path.extension(file).toLowerCase();
+            if (ext == "png" || ext == "jpg" || ext == "jpeg") {
+                var img = Image.fromFile(Path.join([folder, file]));
+                images.push({
+                    name: Path.withoutExtension(file),
+                    image: img,
+                    x: 0,
+                    y: 0,
+                    width: img.width,
+                    height: img.height
+                });
+            }
+        }
+    }
+
+    public function packImages():Void {
+        // Simple packing: arrange images in a single row
+        var x = 0;
+        var maxHeight = 0;
+        for (img in images) {
+            img.x = x;
+            img.y = 0;
+            x += img.width;
+            if (img.height > maxHeight) maxHeight = img.height;
+        }
+        sheetWidth = x;
+        sheetHeight = maxHeight;
+    }
+
+    public function createSpritesheet():Image {
+        var buffer = new ImageBuffer(sheetWidth, sheetHeight, null, 0x00000000);
+        var sheet = new Image(buffer);
+        for (img in images) {
+            sheet.draw(img.image, img.x, img.y);
+        }
+        return sheet;
+    }
+
+    public function saveSpritesheet(path:String):Void {
+        var sheet = createSpritesheet();
+        sheet.encode("png").saveToFile(path);
+    }
+
+    public function saveXML(path:String):Void {
+        var xml = '<TextureAtlas imagePath="' + Path.withoutDirectory(path) + '">';
+        for (img in images) {
+            xml += '<SubTexture name="' + img.name + '" x="' + img.x + '" y="' + img.y + '" width="' + img.width + '" height="' + img.height + '"/>';
+        }
+        xml += '</TextureAtlas>';
+        File.saveContent(path, xml);
+    }
+}
