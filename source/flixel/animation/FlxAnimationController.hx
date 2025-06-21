db8cf3596a086d6343001b528ab8c8eca6449027
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2025-06-20 21:51:17 -0400

    Update FlxAnimationController.hx

--- source/flixel/animation/FlxAnimationController.hx
+++ source/flixel/animation/FlxAnimationController.hx
@@ -772,7 +772,7 @@ class FlxAnimationController implements IFlxDestroyable
 		final name = frames[0].name;
 		final postIndex = name.indexOf(".", prefix.length);
 		final suffix = name.substring(postIndex == -1 ? name.length : postIndex, name.length);
-		FlxFrame.sort(frames, prefix, suffix);
+		FlxFrame.sort(frames, prefix.length, suffix);
 		
 		for (frame in frames)
 		{
