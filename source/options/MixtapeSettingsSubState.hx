9b70b766c7b18e1a225737474e634d1560c5e4bd
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2025-06-22 21:27:42 -0400

    Update MixtapeSettingsSubState.hx

--- source/options/MixtapeSettingsSubState.hx
+++ source/options/MixtapeSettingsSubState.hx
@@ -248,7 +248,7 @@ class MixtapeSettingsSubState extends BaseOptionsMenu
 			STRING, 
 			["Light", "Dark"]);
 		option.displayFormat = '< %v >';
-		onChange = function() Achievements.unlock('much_better');
+		option.onChange = function() Achievements.unlock('much_better');
 		addOption(option);
 		
 		var option:Option = new Option('---MISC.---',
