0d65f5fa192bd90d81f9207335f1b2ecedc5f705
Yutamon <79994593+Yuta12342@users.noreply.github.com>
2025-09-16 18:24:40 -0400

    Update APItem.hx

--- source/archipelago/APItem.hx
+++ source/archipelago/APItem.hx
@@ -313,7 +313,7 @@ class APItem {
                     hasPocketLens = true;
                     trace("Pocket Lens acquired! Player can now view AP items.");
                     popup('You can now view your AP items and stats!', "You got a Pocket Lens!");
-                    
+
                     // If currently in APItemsViewerState, reset it to refresh with new data
                     if (Std.is(FlxG.state, archipelago.APItemsViewerState)) {
                         FlxG.resetState();
