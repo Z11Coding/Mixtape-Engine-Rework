c284d506cb4047ef9ff2f41ce7050e035d9f6284
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2025-09-30 19:46:17 -0400

    game over fix

--- setup/windows.bat
+++ setup/windows.bat
@@ -45,28 +45,3 @@ haxelib git tentools https://github.com/TentaRJ/tentools.git
 haxelib git systools https://github.com/haya3218/systools
 haxelib run lime rebuild systools windows
 echo Finished!
-pause
-
-REM Ask user if they want to ensure the exact required versions
-set /p ensure_versions="Do you want to ensure the exact required library versions? (y/n): "
-if /i "%ensure_versions%"=="y" (
-    haxelib set lime 8.2.2
-    haxelib set openfl 9.4.1
-    haxelib set flixel 5.6.2
-    haxelib set flixel-addons 3.3.2
-    haxelib set flixel-tools 1.5.1
-    haxelib set hscript-iris 1.1.3
-    haxelib set tjson 1.4.0
-    haxelib set hxdiscord_rpc 1.3.0
-    haxelib set hxvlc 2.0.1
-    haxelib set helder.set 0.3.1
-    haxelib set yaml 2.0.1
-    haxelib set hxWebSockets git
-    haxelib set haxe-concurrent 5.1.3
-    haxelib set actuate 1.9.0
-    haxelib set flixel-ui 2.6.1
-    haxelib set hscript 2.5.0
-    haxelib set noisehx 0.0.1
-    REM Add more haxelib set commands here if you add more versioned libraries above
-    echo All required library versions have been set.
-)
