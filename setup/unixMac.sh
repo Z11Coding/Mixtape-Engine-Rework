279e8e2c98a24d3576cb608f297e24b6029def70
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2025-09-30 10:10:39 -0400

    install versions fix

--- setup/unixMac.sh
+++ setup/unixMac.sh
@@ -8,25 +8,33 @@ echo Makking the main haxelib and setuping folder in same time..
 mkdir ~/haxelib && haxelib setup ~/haxelib
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
-haxelib install flixel 5.6.1
+haxelib install lime 8.2.2
+haxelib install openfl 9.4.1
+haxelib install flixel 5.6.2
 haxelib install flixel-addons 3.3.2
 haxelib install flixel-tools 1.5.1
 haxelib install hscript-iris 1.1.3
 haxelib install tjson 1.4.0
-haxelib install hxdiscord_rpc 1.2.4
+haxelib install hxdiscord_rpc 1.3.0
 haxelib install hxvlc 2.0.1 --skip-dependencies
 haxelib install helder.set 0.3.1
 haxelib install yaml 2.0.1
-haxelib install hxWebSockets 1.4.0
 haxelib install haxe-concurrent 5.1.3
 haxelib install actuate 1.9.0
 haxelib install flixel-ui 2.6.1
 haxelib install hscript 2.5.0
-haxelib install noisehx
+haxelib install noisehx 0.0.1
+haxelib install haxeui-core 1.7.0
+haxelib install haxeui-flixel 1.7.0
+haxelib install funkin-modchart 1.2.3
+haxelib install deflatex 1.0.0
+haxelib install crypto 1.0.4
+haxelib install openflCamera 1.0.7
+haxelib install openflMicrophone 1.0.1
 haxelib set lime 8.1.2
 haxelib set openfl 9.3.3
+haxelib git flxsoundfilters https://github.com/TheZoroForce240/FlxSoundFilters
+haxelib git hxWebSockets https://github.com/ianharrigan/hxWebSockets
 haxelib git linc_dialogs https://github.com/snowkit/linc_dialogs.git
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
--- setup/windows.bat
+++ setup/windows.bat
@@ -15,7 +15,6 @@ haxelib install hxdiscord_rpc 1.3.0
 haxelib install hxvlc 2.0.1 --skip-dependencies
 haxelib install helder.set 0.3.1
 haxelib install yaml 2.0.1
-haxelib git hxWebSockets https://github.com/ianharrigan/hxWebSockets
 haxelib install haxe-concurrent 5.1.3
 haxelib install actuate 1.9.0
 haxelib install flixel-ui 2.6.1
@@ -23,8 +22,15 @@ haxelib install hscript 2.5.0
 haxelib install noisehx 0.0.1
 haxelib install haxeui-core 1.7.0
 haxelib install haxeui-flixel 1.7.0
+haxelib install funkin-modchart 1.2.3
+haxelib install deflatex 1.0.0
+haxelib install crypto 1.0.4
+haxelib install openflCamera 1.0.7
+haxelib install openflMicrophone 1.0.1
 haxelib set lime 8.2.2
 haxelib set openfl 9.4.1
+haxelib git flxsoundfilters https://github.com/TheZoroForce240/FlxSoundFilters
+haxelib git hxWebSockets https://github.com/ianharrigan/hxWebSockets
 haxelib git funkin-modchart https://github.com/TheoDevelops/FunkinModchart
 haxelib git linc_dialogs https://github.com/snowkit/linc_dialogs.git
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
