dd9a7504185a969949a68f7539e23291cd2d5178
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2025-10-06 01:07:36 -0400

    a

--- assets/base_game/shared/data/slowdown/spriteBreaker.hx
+++ assets/base_game/shared/data/slowdown/spriteBreaker.hx
@@ -2,24 +2,18 @@ var spriteBreaker:ProxyField;
 var spriteBreaker2:ProxyField;
 function onCreatePost()
 {
-    game.dadField.noteField.alpha = 0;
-	game.dadField.noteField.scrollFactor.set(1, 1);
+    game.dadField.noteField.scrollFactor.set(1, 1);
     game.playerField.noteField.scrollFactor.set(1, 1);
-    
-    spriteBreaker = new ProxyField(game.dadField.noteField);
-	spriteBreaker.cameras = [game.camGame];
-	spriteBreaker.scrollFactor.set(1,1);
-    addBehindDad(spriteBreaker);
 
-    spriteBreaker2 = new ProxyField(game.playerField.noteField);
-	spriteBreaker2.cameras = [game.camOther];
-	spriteBreaker2.scrollFactor.set(1,1);
-    spriteBreaker2.alpha = 0;
-    add(spriteBreaker2);
+    game.dadField.cameras = [game.camGame];
+    game.dadField.noteField.cameras = [game.camGame];
+
+    game.playerField.cameras = [game.camOther];
+    game.playerField.noteField.cameras = [game.camOther];
 }
 
 function onStepHit() {
-    if (curStep == 2544) 
+    if (curStep == 2544)
     {
         game.playerField.noteField.alpha = 0;
         game.playerField.noteField.scrollFactor.set(1, 1);
