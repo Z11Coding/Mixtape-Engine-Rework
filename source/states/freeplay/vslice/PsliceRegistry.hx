ed8ee80a7084a6825b7036672b4e892dcd48a32c
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2025-11-15 00:34:00 -0500

    silly lil mistakes

--- source/states/freeplay/vslice/PsliceRegistry.hx
+++ source/states/freeplay/vslice/PsliceRegistry.hx
@@ -8,17 +8,18 @@ class PsliceRegistry {
     public function new(registryName:String) {
         regPath = 'registry/$registryName';
     }
+
     function readJson(id:String):Dynamic {
         var char_path = Paths.getPath('$regPath/$id.json', TEXT);
         if(!NativeFileSystem.exists(char_path)) return null;
         var text = NativeFileSystem.getContent(char_path);
-
-        return Json.parse(text);// new PlayerData();
+        return Json.parse(text);
     }
+
     function listJsons():Array<String> {
         var char_path = Paths.getPath(regPath);
         var basedCharFiles = NativeFileSystem.readDirectory(char_path);
-        if(char_path == 'mods/$regPath'){
+        if(char_path == 'mods/$regPath') {
             var nativeChars = NativeFileSystem.readDirectory(Paths.getPath(regPath,true));
             basedCharFiles = basedCharFiles.concat(nativeChars);
         }
--- source/states/freeplay/vslice/obj/SngCapsuleData.hx
+++ source/states/freeplay/vslice/obj/SngCapsuleData.hx
@@ -90,20 +90,20 @@ abstract class SngCapsuleData{
 				allowErect = false;
 				freeplayPrevStart = 0;
 				freeplayPrevEnd = 0.2;
-				songStartingBpm = try{backend.Song.getChart(getNativeSongId()+(currentDifficulty.toLowerCase() != "normal" ? currentDifficulty.toLowerCase() : ""), getNativeSongId()).bpm;}catch(e){0;}
+				songStartingBpm = try{backend.Song.getChart(getNativeSongId()+(currentDifficulty.toLowerCase() != "normal" ? currentDifficulty.toLowerCase() : ""), getNativeSongId()).bpm;}catch(e){1;}
 				albumId = meta.freeplay?.album ?? '';
 				instVariants = [];
 
 				songPlayer = 'bf';
 				songWeekName = '';
 			} else {
-				trace("NO METADATA COULD BE LOADED :(\nUSING DEFAULTS SO FREEPLAY DOESN'T HAVE A STROKE AND DIE");
+				//trace("NO METADATA COULD BE LOADED :(\nUSING DEFAULTS SO FREEPLAY DOESN'T HAVE A STROKE AND DIE");
 				difficultyRating = -1;
 				metaAllowNew = true;
 				allowErect = false;
 				freeplayPrevStart = 0;
 				freeplayPrevEnd = 0.2;
-				songStartingBpm = try{backend.Song.getChart(getNativeSongId()+(currentDifficulty.toLowerCase() != "normal" ? currentDifficulty.toLowerCase() : ""), getNativeSongId()).bpm;}catch(e){0;}
+				songStartingBpm = try{backend.Song.getChart(getNativeSongId()+(currentDifficulty.toLowerCase() != "normal" ? currentDifficulty.toLowerCase() : ""), getNativeSongId()).bpm;}catch(e){1;}
 				albumId = 'noCover';
 				instVariants = [];
 
