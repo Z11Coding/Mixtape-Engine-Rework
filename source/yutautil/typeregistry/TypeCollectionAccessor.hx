47b6e97672ca3e62fbf64976022511355b9aec4d
Yutamon <79994593+Yuta12342@users.noreply.github.com>
2025-12-28 06:50:29 -0500

    e

--- source/yutautil/typeregistry/TypeCollectionAccessor.hx
+++ source/yutautil/typeregistry/TypeCollectionAccessor.hx
@@ -2,16 +2,16 @@ package yutautil.typeregistry;
 
 /**
  * Auto-generated type collection data accessor
- * Generated at: 2025-12-27 17:32:58
+ * Generated at: 2025-12-28 06:45:05
  */
 class TypeCollectionAccessor {
-    public static var buildTimestamp:Float = 1.766874663e+012;
+    public static var buildTimestamp:Float = 1.766922242e+012;
     public static var targetPlatform:String = "cpp";
-    public static var classCount:Int = 965;
-    public static var abstractCount:Int = 539;
-    public static var functionCount:Int = 8370;
+    public static var classCount:Int = 972;
+    public static var abstractCount:Int = 541;
+    public static var functionCount:Int = 8434;
     public static var enumCount:Int = 197;
-    public static var typedefCount:Int = 730;
+    public static var typedefCount:Int = 729;
     
     public static function getDataPath():String {
         return "export/builddata/type_collection_compressed.json";
