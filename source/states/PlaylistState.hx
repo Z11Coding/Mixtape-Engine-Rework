38beb7eebda96aeb085f7477a5320a47f9309159
Z11Coding/Gaming <87886392+Z11Coding@users.noreply.github.com>
2026-01-22 22:34:59 -0500

    yay the camera works now :D
    now time to fix EVERYTHING ELSE
    (end me)

--- source/states/PlaylistState.hx
+++ source/states/PlaylistState.hx
@@ -326,7 +326,7 @@ class PlaylistState extends MusicBeatState {
 		if (readyTxt != null)
 			for (i in 0...readyTxt.letters.length) {
 				readyTxt.letters[i].color = FlxColor.fromHSL((((e / 2) / 300 * 360) % 360)+(15*i), 1.0, 0.5*1.0);
-				readyTxt.letters[i].offset.y = (Math.sin((e*0.001) * 2) * 5)+(15*i);
+				readyTxt.letters[i].offset.y = (Math.sin((e*0.001) * 2) * 5 +(15*i));
 			}
 
 		lerpScore = Math.floor(FlxMath.lerp(intendedScore, lerpScore, Math.exp(-elapse * 24)));
